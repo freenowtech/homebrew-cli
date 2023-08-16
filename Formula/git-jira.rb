58ec81b80b63ce60fe08a959b18e83f60da153c3
Fabian Neugart <f.neugart@mytaxi.com>
2023-08-16 19:50:11 +0200

    adding formula for cli

---
+++ Formula/git-jira.rb
@@ -0,0 +1,35 @@
+class GitJira < Formula
+  include Language::Python::Virtualenv
+  
+  desc ""
+  homepage ""
+  url "https://github.com/freenowtech/git-jira.git", :using => :git, :tag => "v0.2.8"
+  sha256 ""
+  license ""
+  
+  depends_on "python@3.11"
+
+  resource "simple-term-menu" do
+    url "https://files.pythonhosted.org/packages/2d/69/31c7d6927338e1deac36e62be5a122e109a0a835a9bf2e30d422c0f3ccc3/simple-term-menu-1.6.1.tar.gz"
+    sha256 "368b4158d1749b868552fb6c054b8301785086c71a7253dac8404cc3cb2d30e8"
+  end
+
+  resource "click" do
+    url "https://files.pythonhosted.org/packages/96/d3/f04c7bfcf5c1862a2a5b845c6b2b360488cf47af55dfa79c98f6a6bf98b5/click-8.1.7.tar.gz"
+    sha256 "ca9853ad459e787e2192211578cc907e7594e294c7ccc834310722b41b9ca6de"
+  end
+  
+  resource "typing-extensions" do
+    url "https://files.pythonhosted.org/packages/3c/8b/0111dd7d6c1478bf83baa1cab85c686426c7a6274119aceb2bd9d35395ad/typing_extensions-4.7.1.tar.gz"
+    sha256 "b75ddc264f0ba5615db7ba217daeb99701ad295353c45f9e95963337ceeeffb2"
+  end
+
+  resource "typer" do
+    url "https://files.pythonhosted.org/packages/5b/49/39f10d0f75886439ab3dac889f14f8ad511982a754e382c9b6ca895b29e9/typer-0.9.0.tar.gz"
+    sha256 "50922fd79aea2f4751a8e0408ff10d2662bd0c8bbfa84755a699f3bada2978b2"
+  end
+  
+  def install
+    virtualenv_install_with_resources
+  end
+end
--- README.md
+++ README.md
@@ -1,10 +1,12 @@
-# Free-now Cli
+# FREENOW CLI
+
+`brew tap free-now/cli "https://github.com/freenowtech/homebrew-cli.git"`
 
 ## How do I install these formulae?
 
-`brew install free-now/cli/<formula>`
+`brew install free-now/cli/git-jira`
 
-Or `brew tap free-now/cli` and then `brew install <formula>`.
+Or `brew tap free-now/cli` and then `brew install git-jira`.
 
 ## Documentation
 
